68404c0de5f5b67be4056cdde117338104c122b7
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-06-07 17:27:55 +0200

    Add variable examples

--- types-basics/main.js
+++ types-basics/main.js
@@ -1,1 +1,11 @@
-alert('hello world');
+let age = 32;
+let year = 2019;
+
+console.log(age, year); // accepts multiple parameters
+
+age = 60;
+
+console.log(age);
+
+const full = 100;
+// full = 50;   Uncaught TypeError: Assignment to constant variable
