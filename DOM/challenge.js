9275d78846d0c39baa4b125398506bb54a7b3936
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-06-08 21:56:28 +0200

    Refactor challenge code

--- DOM/challenge.js
+++ DOM/challenge.js
@@ -1,11 +1,9 @@
 const paragaphs = document.querySelectorAll('p');
 
 paragaphs.forEach(p => {
-    p.innerText.split(' ').forEach(word => {
-        if (word === 'error') {
-            p.classList.add('error');
-        } else if (word === 'success') {
-            p.classList.add('success');
-        }
-    });
+    if (p.innerText.includes('error')) {
+        p.classList.add('error');
+    } else if (p.innerText.includes('success')) {
+        p.classList.add('success');
+    }
 });
