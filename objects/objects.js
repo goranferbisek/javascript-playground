3b64fcd02bc4dd31bd886688a787637deeda7798
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-06-10 22:51:43 +0200

    Use the Math Object

--- objects/objects.js
+++ objects/objects.js
@@ -32,3 +32,21 @@ console.log(typeof user);
 // methods
 user.login();
 user.logBlogs();
+
+// use Math Object
+console.log(Math.PI);
+
+const area = 7.65;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area)); // remove decimals
+
+console.log('A random number');
+
+// decimal between 0 an 1
+const random = Math.random();
+console.log(random);
+
+// a number between 1 an 100
+console.log(Math.round(random * 100));
