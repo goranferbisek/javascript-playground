5918a50de7bdb22a884e92dcb80bfda87a81197f
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-06-17 21:55:32 +0200

    Add functions example

---
+++ functions/functions.js
@@ -0,0 +1,14 @@
+// function expression
+// must be declared before use because it is not hoisted to the top
+const speak = function() {
+    console.log(`good day ${name}`);
+}
+
+greet();
+speak('mario');
+
+// function declaration
+// can be declared anywhere because of hoisting to the top
+function greet() {
+    console.log('hello there');
+}
